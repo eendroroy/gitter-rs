25685e5dcf72268a1b9792ae246e42b0cd0ccf4b
indrajit <eendroroy@gmail.com>
2026-06-10 20:12:28 +0600

    stylize help menu

--- src/command/cli.rs
+++ src/command/cli.rs
@@ -1,7 +1,20 @@
 use clap::{Parser, Subcommand, ValueEnum};
+use clap::builder::Styles;
+use clap::builder::styling::AnsiColor::{Blue, Cyan, Green, Red, Yellow};
+use clap::builder::styling::Color::Ansi;
+use clap::builder::styling::Style;
+
+pub const CLAP_STYLE: Styles = Styles::styled()
+    .header(Style::new().bold().fg_color(Some(Ansi(Green))))
+    .usage(Style::new().bold().fg_color(Some(Ansi(Green))))
+    .literal(Style::new().fg_color(Some(Ansi(Blue))).bold())
+    .placeholder(Style::new().fg_color(Some(Ansi(Cyan))))
+    .error(Style::new().fg_color(Some(Ansi(Red))).bold())
+    .valid(Style::new().fg_color(Some(Ansi(Green))))
+    .invalid(Style::new().fg_color(Some(Ansi(Yellow))));
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, styles=CLAP_STYLE)]
 pub(crate) struct Cli {
     #[command(subcommand)]
     pub(crate) command: Commands,
@@ -12,15 +25,15 @@ pub(crate) struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub(crate) enum Commands {
+    /// List repositories
+    #[clap(alias = "ls")]
+    List,
     /// Run a git command
     #[clap(alias = "g")]
     Git {
         #[arg(trailing_var_arg = true, allow_hyphen_values = true, num_args = 0..)]
         raw_args: Vec<String>,
     },
-    /// List repositories
-    #[clap(alias = "ls")]
-    List,
     /// Run an arbitrary command
     #[clap(alias = "x")]
     Exec {
