79a67e884387c297d94d61d915376e3135702250
indrajit <eendroroy@gmail.com>
2026-06-16 16:06:08 +0600

    update gitterignore help menu

--- src/help/print_gitterignore_help.rs
+++ src/help/print_gitterignore_help.rs
@@ -1,39 +1,30 @@
 use crate::gitter::CLAP_STYLE;
 
+macro_rules! gitterignore_template {
+    () => {
+"\
+{header}Gitterignore File Format{header:#}
+
+{usage}Description:{usage:#}
+  The {literal}.gitterignore{literal:#} file specifies repositories to be ignored by gitter.
+  Each line is a pattern. Empty lines and lines starting with {literal}#{literal:#} are comments.
+  All path should be relative to the .gitterignore file.{}
+
+{header}Patterns:{header:#}
+  Patterns are interpreted based on their format:
+
+  {literal}path/to/repo{literal:#}  - Ignores repositories with the exact relative path or name.
+  {literal}prefix*{literal:#}       - Ignores repositories whose relative path or name starts with a given prefix.
+  {literal}dir_name/*{literal:#}    - Ignores repositories if their immediate sub-directory is an exact name.
+  {literal}dir_prefix*/*{literal:#} - Ignores repositories if their immediate sub-directory starts with a given prefix.
+"
+    };
+}
+
 pub fn print_gitterignore_help() {
     let header = CLAP_STYLE.get_header();
     let usage = CLAP_STYLE.get_usage();
     let literal = CLAP_STYLE.get_literal();
 
-    let padding = 30;
-
-    println!("{header}Gitterignore File Format{header:#}");
-
-    println!("\n{usage}Description:{usage:#}");
-    println!(
-        "  The {literal}.gitterignore{literal:#} file specifies repositories to be ignored by gitter."
-    );
-    println!(
-        "  Each line is a pattern. Empty lines and lines starting with {literal}#{literal:#} are comments."
-    );
-
-    println!("\n{header}Patterns:{header:#}");
-    println!("  Patterns are interpreted based on their format:");
-    println!();
-    println!(
-        "  {: <padding$} - Ignores repositories with the exact relative path or name.",
-        format!("{literal}path/to/repo{literal:#}")
-    );
-    println!(
-        "  {: <padding$} - Ignores repositories whose relative path or name starts with a given prefix.",
-        format!("{literal}prefix*{literal:#}")
-    );
-    println!(
-        "  {: <padding$} - Ignores repositories if their immediate sub-directory (relative to the .gitterignore file) is an exact name.",
-        format!("{literal}dir_name/*{literal:#}")
-    );
-    println!(
-        "  {: <padding$} - Ignores repositories if their immediate sub-directory (relative to the .gitterignore file) starts with a given prefix.",
-        format!("{literal}dir_prefix*/*{literal:#}")
-    );
+    print!(gitterignore_template!(), header = header, usage = usage, literal = literal,);
 }
