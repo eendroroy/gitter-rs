cfba5238e3280e1da2e0878756591def8a2eae99
indrajit <eendroroy@gmail.com>
2026-06-23 13:56:13 +0600

    update macros

--- src/macros/printer.rs
+++ src/macros/printer.rs
@@ -1,23 +1,23 @@
 #[macro_export]
 macro_rules! print_error {
     () => {
-        use crate::style::ERROR;
+        use $crate::style::ERROR;
         eprintln!("{}", *ERROR);
     };
     ($($arg:tt)*) => {
-        use crate::style::ERROR;
+        use $crate::style::ERROR;
         eprintln!("{}{}", *ERROR, format_args!($($arg)*));
     };
 }
 
 #[macro_export]
 macro_rules! print_warn {
-    () => {
-        use crate::style::WARN;
+    () => {c
+        use $crate::style::WARN;
         eprintln!("{}", *WARN);
     };
     ($($arg:tt)*) => {
-        use crate::style::WARN;
+        use $crate::style::WARN;
         eprintln!("{}{}", *WARN, format_args!($($arg)*));
     };
 }
