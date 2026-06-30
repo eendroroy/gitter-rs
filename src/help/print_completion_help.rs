6173bf97338878a2393eae9c1531ecd0475638df
indrajit <eendroroy@gmail.com>
2026-06-30 12:12:21 +0600

    refactor helps

--- src/help/print_completion_help.rs
+++ src/help/print_completion_help.rs
@@ -10,20 +10,20 @@ macro_rules! completion_template {
 
 {header}Quick Setup Commands{header:#}
 
-  - {usage}Bash:{usage:#}       File:   {literal}gitter completion bash > ~/.bash_completion.d/gitter.bash{literal:#}
-                Inline: {literal}eval \"$(gitter completion bash)\"{literal:#}
+  - {usage}Bash:{usage:#}       File:   {literal}gitter completion --bash > ~/.bash_completion.d/gitter.bash{literal:#}
+                Inline: {literal}eval \"$(gitter completion --bash)\"{literal:#}
 
-  - {usage}Zsh:{usage:#}        File:   {literal}gitter completion zsh > ~/.zfunc/_gitter{literal:#}
-                Inline: {literal}eval \"$(gitter completion zsh)\"{literal:#}
+  - {usage}Zsh:{usage:#}        File:   {literal}gitter completion --zsh > ~/.zfunc/_gitter{literal:#}
+                Inline: {literal}eval \"$(gitter completion --zsh)\"{literal:#}
 
-  - {usage}Fish:{usage:#}       File:   {literal}gitter completion fish > ~/.config/fish/completions/gitter.fish{literal:#}
-                Inline: {literal}gitter completion fish | source{literal:#}
+  - {usage}Fish:{usage:#}       File:   {literal}gitter completion --fish > ~/.config/fish/completions/gitter.fish{literal:#}
+                Inline: {literal}gitter completion --fish | source{literal:#}
 
-  - {usage}Elvish:{usage:#}     File:   {literal}gitter completion elvish > ~/.config/elvish/lib/gitter.elv{literal:#}
-                Inline: {literal}gitter completion elvish | eval{literal:#}
+  - {usage}Elvish:{usage:#}     File:   {literal}gitter completion --elvish > ~/.config/elvish/lib/gitter.elv{literal:#}
+                Inline: {literal}gitter completion --elvish | eval{literal:#}
 
-  - {usage}PowerShell:{usage:#} File:   {literal}gitter completion powershell >> $PROFILE{literal:#}
-                Inline: {literal}gitter completion powershell | Out-String | Invoke-Expression{literal:#}
+  - {usage}PowerShell:{usage:#} File:   {literal}gitter completion --powershell >> $PROFILE{literal:#}
+                Inline: {literal}gitter completion --powershell | Out-String | Invoke-Expression{literal:#}
 "
     };
 }
