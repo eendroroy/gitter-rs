dc9b166038c87b4e47042666781161627a056f47
indrajit <eendroroy@gmail.com>
2026-07-01 17:28:46 +0600

    fix get_default_shell

--- src/cli/processor/helper/get_default_shell.rs
+++ src/cli/processor/helper/get_default_shell.rs
@@ -1,5 +1,7 @@
 use std::env;
 
 pub fn get_default_shell() -> String {
-    env::var("SHELL").unwrap_or_else(|_| "/bin/sh".to_string())
+    let shell = env::var("SHELL").unwrap_or_else(|_| "/bin/sh".to_string());
+    shell.split("/").last().unwrap().to_string()
 }
+
