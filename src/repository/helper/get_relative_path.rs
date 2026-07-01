2674acf3e700c1d29df45ac7bb9a32eccc4b34e2
indrajit <eendroroy@gmail.com>
2026-07-01 15:07:58 +0600

    simplify get_relative_path

--- src/repository/helper/get_relative_path.rs
+++ src/repository/helper/get_relative_path.rs
@@ -1,24 +1,23 @@
-use pathdiff::diff_paths;
 use std::path::Path;
 
 pub fn get_relative_path(path: &Path, base_path: &Path) -> String {
-    let parent = path.parent().unwrap();
-
-    let mut result = if let Some(rel_path) = diff_paths(parent, base_path) {
-        let s = rel_path.to_string_lossy().replace('\\', "/");
-
-        if s.is_empty() || s == "." {
-            "./".to_string()
-        } else if s.starts_with("../") || s == ".." {
-            s
-        } else {
-            format!("./{}", s)
-        }
+    let mut result = if path == base_path {
+        String::default()
     } else {
-        parent.to_string_lossy().into_owned()
+        path.parent()
+            .unwrap()
+            .strip_prefix(base_path)
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string()
     };
 
-    if !result.ends_with('/') {
+    if result != "" && !result.starts_with("./") {
+        result = format!("./{}", result);
+    }
+
+    if result != "" && !result.ends_with('/') {
         result.push('/');
     }
 
